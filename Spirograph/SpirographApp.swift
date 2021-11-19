e983802f7ef0634e917658c4c0c522ce797f9b8c
Daisy Guerrero <dguerrero1@bsd220.org>
2021-11-19 10:46:49 -0600

    Initial Commit

---
+++ Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographApp.swift
+//  Spirograph
+//
+//  Created by Student on 11/19/21.
+//
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
